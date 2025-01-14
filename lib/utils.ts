69751d604d9a8a5f342cbd71f9e643b9551e4645
hernandezraymondm <hernandezraymondm@gmail.com>
2025-01-14 19:24:58 +0800

    update general utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
+}
+
+export function formatDate(date: string): string {
+  return new Date(date).toLocaleDateString("en-PH", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
+
+export function formatDateTime(date: string): string {
+  return new Date(date).toLocaleString("en-PH", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 }
