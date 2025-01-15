3e999240b35108563b10b3d91b178f5bcec25c77
hernandezraymondm <hernandezraymondm@gmail.com>
2025-01-15 13:50:45 +0800

    add nextauth module augmentation

---
+++ next-auth.d.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { UserRole } from "@prisma/client";
+
+declare module "next-auth" {
+  interface User {
+    role: UserRole;
+  }
+
+  interface Session {
+    user: {
+      role: UserRole;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role: UserRole;
+  }
+}
