5f3172bee1341644b0476b88fcd24e64e2c611ef
hernandezraymondm <hernandezraymondm@gmail.com>
2025-01-15 13:51:37 +0800

    extend auth session to roles & id

--- auth.ts
+++ auth.ts
@@ -1,11 +1,30 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
-
 import { db } from "@/lib/db";
+import { Adapter } from "next-auth/adapters";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(db),
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      console.log({ token, user, account });
+
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db) as Adapter,
   session: { strategy: "jwt" },
   ...authConfig,
 });
