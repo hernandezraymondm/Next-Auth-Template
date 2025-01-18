ebc86bbe1fc95c0580fe00e2f33495535be6c1b7
hernandezraymondm <rayhernandez.dev@gmail.com>
2025-01-18 12:43:46 +0800

    add email template for account locked notification

--- lib/mail.ts
+++ lib/mail.ts
@@ -12,3 +12,13 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     html: `<p>Keep your Account secure by verifying your email address. <a href="${confirmLink}">Verify Email</a></p>`,
   });
 };
+
+export const sendLockoutEmail = async (email: string) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Your Account Has Been Locked",
+    html: `<p>Your account has been locked due to multiple failed login attempts.</p>
+    <p> If this was you, you don’t need to do anything. If not, please secure your account.</p>`,
+  });
+};
