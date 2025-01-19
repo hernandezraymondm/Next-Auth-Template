a35e5000e64d7c76392aa418ae7021425330f0cb
hernandezraymondm <hernandezraymondm@gmail.com>
2025-01-20 03:48:30 +0800

    update mail template

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,17 +2,28 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `${process.env.BASE_URL}/auth/email-verification?token=${token}`;
+export const sendVerificationEmail = async (
+  email: string,
+  token: string,
+  code: string,
+  expires: string
+) => {
+  const confirmLink = `${process.env.BASE_URL}/auth/verify-email?token=${token}&expires=${expires}&email=${email}`;
 
+  // Send verification email
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
     subject: "Verify your email",
-    html: `<p>Keep your Account secure by verifying your email address. <a href="${confirmLink}">Verify Email</a></p>`,
+    html: `
+      <p>Keep your account secure by verifying your email address.</p>
+      <p>Click the link to verify: <a href="${confirmLink}">Verify Email</a></p>
+      <p>Then enter this 6-digit code: <strong>${code}</strong></p>
+    `,
   });
 };
 
+// Lockout email alert
 export const sendLockoutEmail = async (email: string) => {
   await resend.emails.send({
     from: "onboarding@resend.dev",
