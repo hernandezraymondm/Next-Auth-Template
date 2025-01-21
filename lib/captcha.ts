8d44f24be22ae9caf3a563b15fff4efe4e6bb9c6
hernandezraymondm <hernandezraymondm@gmail.com>
2025-01-21 16:53:26 +0800

    separate captcha logic for future updates

---
+++ lib/captcha.ts
@@ -0,0 +1,18 @@
+export const verifyReCAPTCHA = async (token: string): Promise<boolean> => {
+  const response = await fetch(
+    "https://www.google.com/recaptcha/api/siteverify",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        secret: process.env.RECAPTCHA_SECRET_KEY!,
+        response: token,
+      }).toString(),
+    }
+  );
+
+  const data = await response.json();
+  return data.success;
+};
+
+// TODO: add Cloudflare Turnstile, hCaptcha CAPTCHA options
