d26c0f8909fdb33846a14ab6dd44874cabce259f
hernandezraymondm <hernandezraymondm@gmail.com>
2025-02-05 08:56:13 +0800

    fix routing error

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,13 +1,14 @@
-import React, { ReactNode } from "react";
 import { Navbar } from "./_components/navbar";
 
-const ProtectedLayout = ({ children }: { children: ReactNode }) => {
+export default function ProtectedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-primary to-neutral h-full w-full flex flex-col gap-y-10 items-center justify-center">
+    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <Navbar />
       {children}
     </div>
   );
-};
-
-export default ProtectedLayout;
+}
